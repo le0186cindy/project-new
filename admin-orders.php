b731b28daf55c159b56beab28f2854d094fc7087
SenPr <cindy.t.le-1@ou.edu>
2024-12-13 08:50:55 -0600

    a

---
+++ admin-orders.php
@@ -0,0 +1,35 @@
+<?php
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+    if (!isset($_SESSION['admin_user'])) {
+        header("Location: scripts/login-admin.php");
+        exit();
+    }
+?>
+<html lang="en">
+<?php
+    $title = "Admin Dashboard";
+    include 'components/head.php';
+?>
+
+<body class="bg-secondary">
+    <div class="h-100 mt-2">
+        <div class="d-flex justify-content-center gap-3">
+            <a class="btn btn-info" role="button" href="/admin.php">Home</a>
+            <a class="btn btn-info" role="button" href="/admin-images.php">Images</a>
+            <a class="btn btn-info" role="button" href="/admin-products.php">Products</a>
+            <a class="btn btn-info" role="button" href="/admin-orders.php">Orders</a>
+        </div>
+    
+
+    <?php 
+        include 'components/admin/orders.php';
+    ?>
+    </div>
+</body>
+
+<?php
+    include 'components/footer.php';
+?>
+</html>
